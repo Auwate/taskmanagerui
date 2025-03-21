c934ee1ab7a1977e844fbdc63fcf38baebae99f2
Auwate <auwate1@gmail.com>
2025-03-21 17:18:23 -0400

    Commit:
    # Description:
    - Added login functionality

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -2,10 +2,12 @@ import "../../styles/login.css"
 import "../../styles/global.css"
 
 import { useEffect } from "react"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
 
     const BACKEND = process.env.NEXT_PUBLIC_BACKEND_SERVER;
+    const router = useRouter();
 
     useEffect(() => {
 
@@ -14,6 +16,10 @@ export default function Home() {
             try {
 
                 const response = fetch(`${BACKEND}/api/tasks`)
+
+            } catch (error) {
+
+                router.push("/auth/login")
 
             }
 
