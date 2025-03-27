eab5fa3006f7c9a077d1020fcafec99438640527
Auwate <auwate1@gmail.com>
2025-03-26 20:06:13 -0400

    Commit

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -3,7 +3,7 @@
 import "../../styles/login.css"
 import "../../styles/global.css"
 
-import { useEffect, useState } from "react"
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
 import { useRouter } from "next/navigation";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
@@ -15,7 +15,7 @@ interface Color {
 }
 
 interface Tag {
-    name: string,
+    name: string | undefined,
     color: Color
 }
 
@@ -24,7 +24,7 @@ interface Task {
     name: string,
     description: string,
     priority: number,
-    tag: Tag | undefined
+    tag: Tag
 }
 
 export default function Home() {
@@ -34,6 +34,13 @@ export default function Home() {
 
     const [tasks, setTasks] = useState<Task[]>([]);
     const [index, setIndex] = useState(0);
+
+    const [isCreating, setIsCreating] = useState<boolean>(false);
+
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [color, setColor] = useState<Color>({red: 0, green: 0, blue: 0});
+    const [priority, setPriority] = useState(1);
 
     useEffect(() => {
 
@@ -74,17 +81,58 @@ export default function Home() {
     }, []);
 
     const handleDecrement = () => {
-        if (index > 0) {
-            setIndex((index) => index-1)
-        }
+        setIndex((index) => index-1 < 0 ? tasks.length-1 : index-1);
+        setIsCreating(false);
     }
+
     const handleIncrement = () => {
-        if (index < tasks.length-1) {
-            setIndex((index) => index+1)
-        }
+        setIndex((index) => index+1 > tasks.length-1 ? 0 : index+1);
+        setIsCreating(false);
     }
+
     const handleCreate = (task: Task) => {
-        setTasks((tasks) => tasks.concat(task))
+        setTasks((tasks) => tasks.concat(task));
+    }
+
+    const handleSubmit = (form: FormEvent<HTMLFormElement>) => {
+
+        form.preventDefault();
+
+        if (title === undefined || description === undefined) {
+            return;
+        }
+
+        handleCreate({id: 0, name: title, description: description, priority: priority, tag: {
+            name: undefined,
+            color: color
+        }})
+
+        setIsCreating(false);
+
+    }
+
+    const handleColorChange = (event: ChangeEvent<HTMLInputElement>) => {
+
+        const val: string = event.target.value;
+
+        const parseHex = (hex: string) => {
+            return parseInt(hex, 16)
+        }
+
+        setColor((prevColor) => (
+            prevColor = {
+                red: parseHex(val.slice(1,3)),
+                green: parseHex(val.slice(3,5)),
+                blue: parseHex(val.slice(5,7))
+            }
+        ));
+
+    }
+
+    const handlePriorityChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (!isNaN(parseInt(event.target.value))) {
+            setPriority(parseInt(event.target.value))
+        }
     }
 
     return (
@@ -92,23 +140,72 @@ export default function Home() {
     <main className="flex-grow flex flex-row mx-12 mb-4 justify-evenly items-center">
         <div className="md:flex hidden w-1/2 justify-center items-center space-x-8">
             <FaArrowAltCircleLeft className="icon" onClick={handleDecrement} />
-            <div className="max-w-[328px] min-w-[328px] min-h-[450px] my-4 h-full flex flex-col justify-center items-center shadow-2xl rounded-2xl bg-white">
-                <div className="w-[90%] h-3/4 flex flex-col space-y-8 p-4">
-                    <div className="flex flex-col min-w-[100%]">
-                        <p className="text-[#b3b3b3] pointer-events-none mb-4">Title</p>
-                        <p>{tasks.length > 0 ? tasks[index].name : "Tasks are empty..."}</p>
-                    </div>
-                    <div className="w-[100%] my-6 border border-px border-[#b3b3b3]"></div>
-                    <div className="flex flex-col min-w-[100%]">
-                        <p className="text-[#b3b3b3] pointer-events-none mb-4">Description</p>
-                        <p>{tasks.length > 0 ? tasks[index].description : "Begin writing your first task by clicking the Create button on the right."}</p>
+            <form method="POST" onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
+                <div
+                    className={`max-w-[328px] min-w-[328px] min-h-[450px] my-4 h-full flex flex-col justify-center items-center shadow-2xl rounded-2xl bg-white`}
+                    style={{boxShadow: `1px 2px 10px ${isCreating ? 
+                        "rgb(" + color.red + "," + color.green + "," + color.blue + ")" :
+                            tasks.length > 0 ?
+                                "rgb(" + tasks[index].tag.color.red + "," + tasks[index].tag.color.green + "," + tasks[index].tag.color.blue + ")" :
+                                "black"}`}}
+                >
+                    <div className="w-[90%] h-3/4 flex flex-col space-y-8 p-4">
+                        <div className="flex flex-col min-w-[100%]">
+                            <p className="text-[#b3b3b3] pointer-events-none mb-4">{isCreating ? "What is the title of your task?" : "Title"}</p>
+                            <p className={`${isCreating ? "hidden" : "block"}`}>{tasks.length > 0 ? tasks[index].name : "Tasks are empty..."}</p>
+                            <textarea
+                                id="title"
+                                name="title"
+                                className={`${isCreating ? "block" : "hidden"} resize-none border border-px rounded-xl py-4 px-3 h-[60px]`}
+                                placeholder=" "
+                                aria-label="title field"
+                                onChange={(event) => setTitle(event.target.value)}
+                            ></textarea>
+                        </div>
+                        <div className={`${isCreating ? "hidden" : "block"} w-[100%] my-6 border border-px border-[#b3b3b3]`}></div>
+                        <div className="flex flex-col min-w-[100%]">
+                            <p className="text-[#b3b3b3] pointer-events-none mb-4">{isCreating ? "What is the description of your task?" : "Description"}</p>
+                            <p className={`${isCreating ? "hidden" : "block"}`}>{tasks.length > 0 ? tasks[index].description : "Begin writing your first task by clicking the Create button on the right."}</p>
+                            <textarea
+                                id="description"
+                                name="description"
+                                className={`${isCreating ? "block" : "hidden"} resize-none border border-px rounded-xl py-4 px-3 h-[60px]`}
+                                placeholder=" "
+                                aria-label="description field"
+                                onChange={(event) => setDescription(event.target.value)}
+                            ></textarea>
+                        </div>
+                        <div className={`${isCreating ? "block" : "hidden"} w-[100%] my-1 border border-px border-[#b3b3b3]`}></div>
+                        <div className={`${isCreating ? "block" : "hidden"} flex justify-around items-center`}>
+                            <div className="flex flex-col">
+                                <p className="text-[#b3b3b3] pointer-events-none mb-4">Color?</p>
+                                <input
+                                    type="color"
+                                    className="rounded-xl w-[50px] h-[50px]"
+                                    onChange={handleColorChange}
+                                ></input>
+                            </div>
+                            <div className={`${isCreating ? "block" : "hidden"} h-[80px] my-6 border border-px border-[#b3b3b3]`}></div>
+                            <div className="flex flex-col">
+                                <p className="text-[#b3b3b3] pointer-events-none mb-4">Priority?</p>
+                                <input
+                                    type="number"
+                                    className="rounded-xl w-[70px] h-[50px] px-4 border border-px"
+                                    min={1}
+                                    onChange={handlePriorityChange}
+                                ></input>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div className={`${isCreating ? "block" : "hidden"} mt-4`}>
+                    <button type="submit" className="btn-secondary w-[13vw] cursor-pointer">Submit</button>
+                </div>
+            </form>
             <FaArrowAltCircleRight className="icon" onClick={handleIncrement} />
         </div>
         <div className="hidden md:flex w-1/3 flex-col justify-center items-center space-y-4">
-            <button onClick={() => (handleCreate({id: 1, name: "Temp", description: "Temp", priority: 1, tag: undefined}))} className="btn-secondary w-[13vw] cursor-pointer">Create</button>
+            <button onClick={() => (setIsCreating(() => !isCreating))} className="btn-secondary w-[13vw] cursor-pointer">Create</button>
             <button className="btn-secondary w-[13vw] cursor-pointer">Delete</button>
             <button className="btn-secondary w-[13vw] cursor-pointer">Modify</button>
         </div>
